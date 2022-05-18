84066b1404ac4afa746fd917cd0a33751f53061b
dj <pandora2948@naver.com>
2022-05-18 16:19:34 +0900

    fix: 조건부 랜더링 오류 수정

--- src/components/Encryption/Wrapper/KeyWrapper.js
+++ src/components/Encryption/Wrapper/KeyWrapper.js
@@ -3,18 +3,18 @@ import KeyInput from "./KeyValue/Inputs/KeyInput";
 import KeyResult from "./KeyValue/Outputs/KeyResult";
 
 const Wrapper = styled.div`
-    margin-top: 2rem;
-    display: flex;
-    flex-flow: column wrap;
-    justify-content: center;
-    ${({ visibility }) => visibility};
-  `;
+  margin-top: 2rem;
+  display: flex;
+  flex-flow: column wrap;
+  justify-content: center;
+  ${({ visibility }) => visibility};
+`;
 
 const KeyWrapper = ({ keyValue, setKeyValue, visibility }) => {
   return (
     <Wrapper visibility={visibility}>
       <KeyInput setKeyValue={setKeyValue} />
-      {keyValue.isEmpty && <KeyResult keyValue={keyValue} />}
+      {!keyValue.isEmpty && <KeyResult keyValue={keyValue} />}
     </Wrapper>
   );
 };
--- src/components/Encryption/Wrapper/PlainTextWrapper.js
+++ src/components/Encryption/Wrapper/PlainTextWrapper.js
@@ -4,19 +4,19 @@ import PlainTextInput from "./PlainText/Inputs/PlainTextInput";
 import PlainTextResult from "./PlainText/Outputs/PlainTextResult";
 
 const Wrapper = styled.div`
-    color: ${variables.colors.subWhiteColor};
-    margin-top: 2rem;
-    display: flex;
-    flex-flow: column wrap;
-    justify-content: center;
-    ${({ visibility }) => visibility};
-  `;
+  color: ${variables.colors.subWhiteColor};
+  margin-top: 2rem;
+  display: flex;
+  flex-flow: column wrap;
+  justify-content: center;
+  ${({ visibility }) => visibility};
+`;
 
 const PlainTextWrapper = ({ plainText, setPlainText, visibility }) => {
   return (
     <Wrapper visibility={visibility}>
       <PlainTextInput setPlainText={setPlainText} />
-      {plainText.isEmpty && <PlainTextResult plainText={plainText} />}
+      {!plainText.isEmpty && <PlainTextResult plainText={plainText} />}
     </Wrapper>
   );
 };
