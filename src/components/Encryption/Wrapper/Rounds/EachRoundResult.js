236e4b30cc214bd5a644ad78ef496f85a2c3e938
Baek Dongjae <80594323+pandora2948@users.noreply.github.com>
2022-05-23 22:06:44 +0900

    DES 알고리즘 16라운드 리펙터링 (#7)
    * refactor: 기존 16라운드 로직 수정 및 프로젝트 구조 개선
    
    * feat: 16라운드 각 결과 출력

---
+++ src/components/Encryption/Wrapper/Rounds/EachRoundResult.js
@@ -0,0 +1,120 @@
+import styled from "@emotion/styled";
+import mixins from "assets/mixins";
+import variables from "assets/variables";
+import { useMemo } from "react";
+
+const Wrapper = styled.div`
+  padding: 1rem 2rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Container = styled.div`
+  ${mixins.PlainTextBox}
+  width: 45%;
+  padding: 1rem 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const RoundTable = styled.table`
+  ${mixins.TableStyle};
+
+  td {
+    border-color: ${variables.colors.encryptedBlueColor};
+  }
+`;
+
+const generateTables = (eachRounds) => {
+  const { leftBlock, rightBlock } = eachRounds;
+
+  const leftElement = [];
+  const rightElement = [];
+
+  for (let i = 1; i < 17; i += 1) {
+    let count = 0;
+    const leftTable = [];
+    const rightTable = [];
+
+    for (let j = 0; j < 4; j += 1) {
+      const leftRow = [];
+      const rightRow = [];
+
+      for (let k = 0; k < 8; k += 1) {
+        const leftData = (
+          <td key={`round-table - td - ${Math.random().toString().slice(3)}`}>
+            {leftBlock[i][count]}
+          </td>
+        );
+
+        const rightData = (
+          <td key={`round-table - td - ${Math.random().toString().slice(3)}`}>
+            {rightBlock[i][count]}
+          </td>
+        );
+
+        leftRow.push(leftData);
+        rightRow.push(rightData);
+        count += 1;
+      }
+
+      leftTable.push(
+        <tr key={`round-table - tr - ${Math.random().toString().slice(3)}`}>
+          {leftRow}
+        </tr>
+      );
+
+      rightTable.push(
+        <tr key={`round-table - tr - ${Math.random().toString().slice(3)}`}>
+          {rightRow}
+        </tr>
+      );
+    }
+
+    leftElement.push(
+      <RoundTable
+        key={`round-table - table - ${Math.random().toString().slice(3)}`}
+      >
+        <tbody
+          key={`round-table -  tbody - ${Math.random().toString().slice(3)}`}
+        >
+          {leftTable}
+        </tbody>
+      </RoundTable>
+    );
+
+    rightElement.push(
+      <RoundTable
+        key={`round-table - table - ${Math.random().toString().slice(3)}`}
+      >
+        <tbody
+          key={`round-table - tbody - ${Math.random().toString().slice(3)}`}
+        >
+          {rightTable}
+        </tbody>
+      </RoundTable>
+    );
+  }
+
+  return { leftElement, rightElement };
+};
+
+const EachRoundResult = ({ eachRounds }) => {
+  const { leftElement, rightElement } = useMemo(
+    () => generateTables(eachRounds),
+    [eachRounds]
+  );
+
+  return (
+    <Wrapper>
+      <Container>{leftElement}</Container>
+      <Container>{rightElement}</Container>
+    </Wrapper>
+  );
+};
+
+export default EachRoundResult;
